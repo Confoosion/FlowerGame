61f9f03b19a843591f2b88cea8c4278a7f530ff8
Jake <jakesales@gmail.com>
2026-04-22 12:41:05 -0700

    Added highlight selecting
    When dragging a seed, the places it can be placed on will be highlighted. The places that are highlighted are the soil slots.

--- Assets/Scripts/IDraggable.cs
+++ Assets/Scripts/IDraggable.cs
@@ -10,6 +10,8 @@ public class IDraggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
     private string GAMEPLAY_NAME = "GAMEPLAY_CANVAS";
     private Transform GAMEPLAY;
 
+    private SelectHighlight activeHighlight;
+
     void Awake()
     {
         draggedImage = GetComponent<Image>();
@@ -27,10 +29,43 @@ public class IDraggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
     public void OnDrag(PointerEventData eventData)
     {
         transform.position = eventData.position + offsetPosition;
+
+        var results = new System.Collections.Generic.List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        if(results.Count > 0)
+        {
+            foreach(var hit in results)
+            {
+                SelectHighlight temp = hit.gameObject.GetComponent<SelectHighlight>();
+                if(temp != null && temp != activeHighlight)
+                {
+                    if(activeHighlight != null)
+                        activeHighlight.DisplayHighlight(false);
+                    
+                    activeHighlight = temp;
+                    activeHighlight.DisplayHighlight(true);
+                }
+            }
+        }
+        else
+        {
+            RemoveHighlight();
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         draggedImage.raycastTarget = true;
+        RemoveHighlight();
+    }
+
+    private void RemoveHighlight()
+    {
+        if(activeHighlight != null)
+        {
+            activeHighlight.DisplayHighlight(false);
+            activeHighlight = null;
+        }
     }
 }
---
+++ Assets/Scripts/SelectHighlight.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectHighlight : MonoBehaviour
+{
+    private GameObject selectHighlight;
+    private string HIGHLIGHT_NAME = "SelectHighlight";
+
+    void Awake()
+    {
+        foreach(Transform child in transform)
+        {
+            if(child.name.Equals(HIGHLIGHT_NAME))
+            {
+                selectHighlight = child.gameObject;
+                break;
+            }
+        }
+        
+        if(selectHighlight == null)
+        {
+            Debug.LogWarning(gameObject + " has no SelectHighlight GameObject attached!");
+            return;
+        }
+        
+        selectHighlight.SetActive(false);
+    }
+
+    public void DisplayHighlight(bool show)
+    {
+        selectHighlight.SetActive(show);
+    }
+}
